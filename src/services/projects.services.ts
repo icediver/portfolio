6683ff9cfb7c67efb9295417226f71200ede080f
icediver <icedivet@live.ru>
2024-08-27 19:05:26 +0300

    debug

--- src/services/projects.services.ts
+++ src/services/projects.services.ts
@@ -2,8 +2,8 @@ import { IProject } from '@/assets/data/project.data';
 
 export const ProjectsService = {
 	async getProjects(): Promise<IProject[]> {
-		return fetch('http://localhost:3000/api/projects').then((res) =>
-			res.json()
+		return fetch('https://portfolio-nu-nine-81.vercel.app/api/projects').then(
+			(res) => res.json()
 		);
 	},
 };
